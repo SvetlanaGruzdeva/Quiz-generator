1e70632014608cec03732897d59f5d160caf84af
Svetlana <gruzdevasvetlana@gmail.com>
2020-05-18 15:41:39 +0200

    Full project as per book

--- GeoQuiz/randomQuizGenerator.py
+++ GeoQuiz/randomQuizGenerator.py
@@ -3,9 +3,6 @@
 # random order, along with the answer key.
 
 import random
-# from pathlib import Path
-
-# print(Path.cwd())
 
 # The quiz data. Keya are states and values are their capitals.
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
@@ -13,12 +10,12 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', '
 # Generate 35 quiz files.
 for quizNum in range(35):
     # Create the quiz and answer key files.
-    quizFile = open(f'capitalquiz{quizMun + 1}.txt', 'w')
+    quizFile = open(f'capitalquiz{quizNum + 1}.txt', 'w')
     answerKeyFile = open(f'capitalquiz_answers{quizNum + 1}.txt', 'w')
     
     # Write out the header for the quiz.
     quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
-    quizFile.write((' '*20) + f'State Capitals Quiz (Form{quizMun + 1})')
+    quizFile.write((' ' * 20) + f'State Capitals Quiz (Form{quizNum + 1})')
     quizFile.write('\n\n')
 
     # Shuffle the order of the states.
@@ -36,9 +33,17 @@ for quizNum in range(35):
         answerOptions = wrongAnswer + [correctAnswer]
         random.shuffle(answerOptions)
 
-        # TODO: Write a question and answer options to the quiz file.
+        # Write a question and answer options to the quiz file.
+        quizFile.write(f'{questionNum + 1}. What is the capital of {states[questionNum]}?\n')
+        for i in range (4):
+            quizFile.write(f"     {'ABCD'[i]}.{ answerOptions[i]}\n")
+        quizFile.write('\n')
 
-        # TODO: Write the answer key to a file.
+        # Write the answer key to a file.
+        answerKeyFile.write(f"{questionNum + 1}.{'ABCD'[answerOptions.index(correctAnswer)]}")
+    quizFile.close()
+    answerKeyFile.close()
+
 
     
 
